4af5536c5776fd2ce28a2240ed2ffb4f5cea5b53
Allysson dos Santos <aallysson0@gmail.com>
2018-08-26 16:13:42 -0300

    Fix error

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true,
+  overlay: true,
   stats: { colors: true }
 }).listen(4000, (err) => {
   if (err) {
